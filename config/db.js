9f1036b701f51047badc5db3b81ff588bc6d839a
nahid <comeback>
2021-06-03 14:31:50 +0600

    Fully functional initial commit

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+const MONGOURI = process.env.MONGOURI;
+
+const InitiateMongoServer = async () => {
+  try {
+    await mongoose.connect(MONGOURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log("Connected to DB !!");
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+};
+
+module.exports = InitiateMongoServer;
